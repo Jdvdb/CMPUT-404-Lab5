a4d8680cbe2301342341a139a782eae7bf58b153
Jordan <jdvandenbruel@gmail.com>
2023-02-06 17:25:54 -0700

    add templates

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 AUTHOR = 'Colourblind Cid'
 SITENAME = 'My'
 SITEURL = ''
-
+THEME = 'templates/mytheme'
 PATH = 'content'
 
 TIMEZONE = 'America/Edmonton'
@@ -10,7 +10,7 @@ DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+# CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
